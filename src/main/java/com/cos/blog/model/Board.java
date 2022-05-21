3bff91d2936539403273cbae129b7ce3198ae5b5
HoYoon-Lee <happylhy0630@naver.com>
2022-05-21 22:35:01 +0900

    :sparkles: User, Board, Reply entity 생성

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,42 @@
+package com.cos.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content; // Summer Note content
+
+    @ColumnDefault("0")
+    private int count;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER)
+    private List<Reply> replies;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,37 @@
+package com.cos.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 30)
+    private String userName;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("'user'")
+    private String role; // admin, user, manager
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
