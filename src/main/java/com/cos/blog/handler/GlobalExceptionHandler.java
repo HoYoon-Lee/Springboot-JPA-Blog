631f5243bb20e389e65444055aa53387e65c4d27
HoYoon-Lee <happylhy0630@naver.com>
2022-05-24 19:12:08 +0900

    :art: AOP를 이용한 에러 처리 작성

---
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.cos.blog.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public String handleArgumentExeption(Exception e){
+        return "<h1>" + e.getMessage() + "</h1>";
+    }
+}
