39e2427ce0dca4dc25f88c497292b5fa1c0eb6e4
HoYoon-Lee <happylhy0630@naver.com>
2022-05-26 17:45:54 +0900

    :art: Error 처리 시 Message 클래스를 사용해 Response Entity를 반환하도록 수정

--- src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.cos.blog.handler;
 
+import com.cos.blog.dto.Message;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public String handleArgumentExeption(Exception e){
-        return "<h1>" + e.getMessage() + "</h1>";
+    public ResponseEntity<Message> handleException(Exception e){
+        System.out.println("check message: " + e.getMessage());
+        return new Message<>(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage())
+                .asResponseEntity();
     }
 }
