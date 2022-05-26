98bddf8c0a9141e7d9ede0f22097542d7a14e751
HoYoon-Lee <happylhy0630@naver.com>
2022-05-26 21:17:02 +0900

    :art: index 함수를 BoardController로 이동

--- src/main/java/com/cos/blog/BlogApplication.java
+++ src/main/java/com/cos/blog/BlogApplication.java
@@ -2,17 +2,11 @@ package com.cos.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
-    }
-
-    @GetMapping
-    public String index(){
-        return "index";
     }
 }
