6a7255a6189858a2087d2c60dda8bf15e05ffc1c
HoYoon-Lee <happylhy0630@naver.com>
2022-05-28 22:52:38 +0900

    :art: Frontend에 Spring Security 적용

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,9 +3,6 @@ let index = {
         $("#btn-save").on("click", ()=>{
             this.save();
         });
-        $("#btn-login").on("click", ()=>{
-            this.login();
-        });
     },
     
     save: function() {
@@ -17,33 +14,12 @@ let index = {
 
         $.ajax({
             type: "POST",
-            url: "/api/user",
+            url: "/auth/joinProc",
             data: JSON.stringify(data),
             contentType: "application/json; charset=utf-8",
             dataType: "json"
         }).done(function(response){
             alert("회원가입이 완료되었습니다.");
-            location.href = "/";
-        }).fail(function(error){
-            alert(JSON.stringify(error))
-        });
-    },
-
-    login: function() {
-        let data = {
-            userName: $("#userName").val(),
-            password: $("#password").val(),
-            email: $("#email").val()
-        };
-
-        $.ajax({
-            type: "POST",
-            url: "/api/user/login",
-            data: JSON.stringify(data),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
-        }).done(function(response){
-            alert("로그인 되었습니다.");
             location.href = "/";
         }).fail(function(error){
             alert(JSON.stringify(error))
--- src/main/webapp/WEB-INF/views/user/loginForm.jsp
+++ src/main/webapp/WEB-INF/views/user/loginForm.jsp
@@ -11,26 +11,19 @@
 <body>
 
 <div class="container">
-    <form>
+    <form action="/auth/loginProc" method="post">
         <div class="form-group">
             <label for="userName">User Name:</label>
-            <input type="text" class="form-control" placeholder="Enter user name" id="userName">
+            <input type="text" name="username" class="form-control" placeholder="Enter user name" id="userName">
         </div>
 
         <div class="form-group">
             <label for="password">Password:</label>
-            <input type="password" class="form-control" placeholder="Enter password" id="password">
+            <input type="password" name="password" class="form-control" placeholder="Enter password" id="password">
         </div>
-        <div class="form-group form-check">
-            <label class="form-check-label">
-                <input class="form-check-input" type="checkbox"> Remember me
-            </label>
-        </div>
+        <button id="btn-login" class="btn btn-primary">로그인</button>
     </form>
-    <button id="btn-login" class="btn btn-primary">로그인</button>
 </div>
-
-<script src="/js/user.js"></script>
 
 <%@ include file="../layout/footer.jsp" %>
 </body>
