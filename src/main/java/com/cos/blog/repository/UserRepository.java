4a9c7a716721a58787948ace1cc5ea589c9d0e12
HoYoon-Lee <happylhy0630@naver.com>
2022-05-28 22:54:43 +0900

    :art: Backend에 Spring Security 적용

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.cos.blog.repository;
 import com.cos.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUserNameAndPassword(String userName, String password);
+    Optional<User> findByUserName(String userName);
 }
--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,8 +1,10 @@
 package com.cos.blog.service;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,14 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
 
     @Transactional
     public void save(User user){
+        String rawPassword = user.getPassword();
+        String encPassword = encoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole(RoleType.USER);
         userRepository.save(user);
-    }
-
-    @Transactional(readOnly = true)
-    public User login(User user){
-        return userRepository.findByUserNameAndPassword(user.getUserName(), user.getPassword());
     }
 }
