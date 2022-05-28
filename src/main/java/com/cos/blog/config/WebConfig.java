81506d3e2119b62ef01ca4527ad48e529f784efd
HoYoon-Lee <happylhy0630@naver.com>
2022-05-28 22:49:32 +0900

    :wrench: /로 접근하면 /board로 리다이렉션하도록 설정

---
+++ src/main/java/com/cos/blog/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.cos.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/board");
+    }
+}
