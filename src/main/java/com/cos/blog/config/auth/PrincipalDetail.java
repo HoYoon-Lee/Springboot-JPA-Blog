f6c91ad3e31ee2f7c1725e28151d021f1567afa5
HoYoon-Lee <happylhy0630@naver.com>
2022-05-28 22:47:45 +0900

    :sparkles: 접근 권한 확인을 위한 PrincipalDetail 작성

---
+++ src/main/java/com/cos/blog/config/auth/PrincipalDetail.java
@@ -0,0 +1,56 @@
+package com.cos.blog.config.auth;
+
+import com.cos.blog.model.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+public class PrincipalDetail implements UserDetails {
+    private final User user;
+
+    public PrincipalDetail(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(() -> "ROLE_" + user.getRole());
+
+        return grantedAuthorities;
+    }
+}
